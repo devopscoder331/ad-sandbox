e6783b8b51dafbf47d9f7c19cb957d2360cea859
devopscoder331 <devopscoder331@gmail.com>
2025-10-04 13:30:48 +0300

    Fix checkers/vboard/

--- checkers/vboard/lib.py
+++ checkers/vboard/lib.py
@@ -1,43 +1,22 @@
-#!/usr/bin/env python3
+import requests
+from checklib import *
 
-import sys
-
-from pathlib import Path
-
-BASE_DIR = Path(__file__).absolute().resolve().parent
-sys.path.insert(0, str(BASE_DIR))
-
-from lib import *
+PORT = 8000
 
 
-class Checker(BaseChecker):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.mch = CheckMachine(self)
+class CheckMachine:
 
-    def action(self, action, *args, **kwargs):
-        try:
-            super().action(action, *args, **kwargs)
-        except requests.exceptions.ConnectionError:
-            self.cquit(Status.DOWN, 'Connection error', 'Got requests connection error')
+    def __init__(self, checker):
+        self.checker = checker
 
-    def check(self):
-        self.mch.ping()
-        self.cquit(Status.OK)
+    def ping(self):
+        r = requests.get(f'http://{self.checker.host}:{PORT}/', timeout=3)
+        self.checker.check_response(r, 'Check failed')
 
-    def put(self, flag_id, flag, vuln):
-        new_id = self.mch.put_flag(flag, vuln)
-        self.cquit(Status.OK, new_id)
+    def put_flag(self, flag, vuln):
+        # TODO: Implement flag placement logic
+        pass
 
-    def get(self, flag_id, flag, vuln):
-        got_flag = self.mch.get_flag(flag_id, flag, vuln)
-        self.assert_eq(got_flag, flag, 'Could not get flag', status=Status.CORRUPT)
-        self.cquit(Status.OK)
-
-
-if __name__ == '__main__':
-    c = Checker(sys.argv[2])
-    try:
-        c.action(sys.argv[1], *sys.argv[3:])
-    except c.get_check_finished_exception():
-        cquit(Status(c.status), c.public, c.private)
+    def get_flag(self, flag_id, flag, vuln):
+        # TODO: Implement flag retrieval logic
+        pass
