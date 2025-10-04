8dfad711e37c2759306d991193427336d5add3b3
devopscoder331 <devopscoder331@gmail.com>
2025-10-04 13:13:02 +0300

    Edited files services/CTR1PANEL

--- services/CTR1PANEL/CleanCTRP.sh
+++ services/CTR1PANEL/CleanCTRP.sh
@@ -3,12 +3,12 @@
 echo "Start clean..."
 echo "Clean Application data"
 
-docker-compose -f compose.yaml down
+docker compose -f docker compose.yml down
 rm -r db
 rm -r logs
 
-docker-compose -f compose.yaml up -d
-docker-compose -f compose.yaml down
-docker-compose -f compose.yaml up -d
+docker compose -f docker compose.yml up -d
+docker compose -f docker compose.yml down
+docker compose -f docker compose.yml up -d
 
 echo "Finish clean..."
--- services/CTR1PANEL/INITCTRP.sh
+++ services/CTR1PANEL/INITCTRP.sh
@@ -3,9 +3,9 @@
 echo "Initial Start service"
 cd /home/uctf/services/CTR1PANEL
 
-docker-compose build
-docker-compose up -d
-docker-compose down
-docker-compose up -d
+docker compose build
+docker compose up -d
+docker compose down
+docker compose up -d
 
 echo "Service started"
